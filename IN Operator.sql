81ea5b76d0d5219c65abf0c628e97c37cdd48444
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-04-04 14:22:27 +0500

    Add files via upload

---
+++ IN Operator.sql
@@ -0,0 +1,6 @@
+		-- records having age equals to 12 or 13;
+-- select * from students where age in(12,13);
+
+		-- records having age not equals to 12 or 13;
+-- select * from students where age not in(12,13);
+
