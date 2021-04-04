e26fc5026e5113d733a972d9e3120c47a23754cf
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-04-04 14:21:51 +0500

    Add files via upload

---
+++ tblConstraints.sql
@@ -0,0 +1,13 @@
+-- create table tblConstraints(
+-- id INT NOT NULL UNIQUE,
+-- name VARCHAR(50) NOT NULL,
+-- age INT NOT NULL CHECK (age<=18),
+-- gender VARCHAR(10) NOT NULL,
+-- phone VARCHAR(10) NOT NULL,
+-- city VARCHAR(10) NOT NULL Default "Karachi");
+
+-- insert into tblconstraints(
+-- id, name, age, gender, phone, city)
+-- values
+-- (1,"Junaid",18,"Male","03133533027","Sukkur"),
+-- (2,"Hassnain",17,"Male","03133533028","Karachi");
