22b8f9efa0794ce34c16acce2d86a7bf6c409708
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-04-04 12:20:03 +0500

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,28 @@
+-- create database w3schools;
+-- use w3schools;
+
+-- create table tblPersonal(
+-- 	id INT,
+--     name varchar(50),
+--     birthdate date,
+--     phone varchar(12),
+--     gender varchar(1)
+-- );
+
+
+		-- Inserting one record
+
+-- insert into tblpersonal(id, name, birthdate, phone, gender) 
+-- values (1,"Junaid Ali","2002-06-26","03133533027","M");
+
+-- insert into tblpersonal(id, name, birthdate, phone, gender) 
+-- values (2,"Jawad","2006-06-26","03133533027","M");
+
+		-- Inserting multiple records
+        
+-- insert into tblpersonal(id, name, birthdate, phone, gender) 
+-- values 
+-- (3,"Sajjad","2009-06-26","03133533032","M"),
+-- (4,"Haseeb","2010-06-26","03133533023","M"),
+-- (5,"Hassnain","2011-06-26","03133533024","M"),
+-- (6,"Kamran","2012-06-26","03133533009","M");
