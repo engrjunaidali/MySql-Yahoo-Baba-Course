24039d15d9fa6d7d12797ba5957bfc1fe7f04831
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-04-04 14:22:18 +0500

    Add files via upload

---
+++ AND OR NOT.sql
@@ -0,0 +1,16 @@
+-- AND OR NOT OPERATORS
+-- AND
+-- OR 
+-- NOT
+
+select * from tblpersonal;
+-- select * from tblpersonal where name = "Junaid Ali" OR "Jawad"; 
+
+-- select * from tblpersonal where id < 3 AND id >1;  
+
+-- select * from tblpersonal where id = 2 or id = 3; 
+
+-- select * from tblpersonal where id <> 1 ;
+
+
+
